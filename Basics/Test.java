aeacc43510544aeb8d96b650967e98d7ea541a20
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-05-10 17:28:09 +0530

    tst

---
+++ Basics/Test.java
@@ -0,0 +1,14 @@
+class A{
+    public static void main(String[] args) {
+        System.out.println("Class A");
+    }
+}
+class B{
+
+}
+class C{
+
+}
+class D{
+    
+}
---
+++ DT/Test.java
@@ -0,0 +1,13 @@
+class Test{
+    public static void main(String[] args) {
+        String ename = "Rahul Gandhi";
+
+        int[] ids ={101,102,103,104,105};
+        
+        System.out.println(ename);
+        System.out.println(ids[0]);
+        System.out.println(ids[1]);
+        System.out.println(ids[2]);
+
+    }
+}
