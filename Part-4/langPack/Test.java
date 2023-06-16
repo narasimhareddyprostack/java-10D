6563b15b127267bf376523c56ac263e4d5e27142
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-06-16 17:57:36 +0530

    resuffle

---
+++ Part-4/langPack/Test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+//import java.lang.String;
+
+public class Test {
+    public static void main(String[] args) {
+        String ename = "Rahul";
+        new ArrayList();
+        Integer i = 100;
+    }
+}
